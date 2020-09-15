4f36738b38022c6935c4873a125980873cfafc68
John Matter <matter@virginia.edu>
2020-09-15 16:09:27 -0400

    Remove SITK from dose-resampling and rewrite similar to previous version

--- rivanna/lymphkill/resample_dose_in_CT_dimensions.py
+++ rivanna/lymphkill/resample_dose_in_CT_dimensions.py
@@ -5,65 +5,87 @@ import numpy as np
 import pickle
 import pydicom
 import nrrd
-import SimpleITK as sitk
 
 from lymphkill.file_utils import find_prefixed_file, find_dicom_directory, find_prefixed_files, load_rtdose_files
-from calc_blood_dose import *
+from lymphkill.calc_blood_dose import *
+
+'''
+Find the first CT frame in the z-direction
+Parameters:
+	ct_infos - A list of loaded CT dicom files
+Returns:
+	The index of the first CT slice in the z-direction
+'''
+def get_first_CT_frame(ct_infos):
+	first = ct_infos[0]
+	for i in ct_infos:
+		if float(i.ImagePositionPatient[2]) < float(first.ImagePositionPatient[2]):
+			first = i
+	return first
 
 '''
 Generate a masks structure given a set of contours and information about the dose files
 Parameters:
-	ct_files - List of files that make up the CT
-	dose_files - List of files containing dose info
+	ct_info - Header information from a CT file (from pydicom.read)
+	dose_info - Header information from a DOSE file (from pydicom.read)
+	dose_grids - dose obtained (from load_rtdose_files)
 Returns:
 	dose - the dose grid resampled in the CT's resolution
 '''
 def resample_dose(
-	ct_files,
-	dose_files):
+	ct_infos,
+	dose_info,
+	dosegrids):
+
+	dose_grids = np.sum(dosegrids,0)
+
+	ct_info = get_first_CT_frame(ct_infos)
+	dim_vol = np.array([ct_info.Rows, ct_info.Columns, len(ct_infos)])
+	dim_dos = np.array([dose_info.Rows, dose_info.Columns, dose_info.NumberOfFrames])
+
+	#Get voxel dimensions
+	dim_voxd = np.array([dose_info.PixelSpacing[0], dose_info.PixelSpacing[1], dose_info.PixelSpacing[0]])
+	dim_voxv = np.array([ct_info.PixelSpacing[0], ct_info.PixelSpacing[1], ct_info.SliceThickness])
+
+	#Get image corners
+	corner_d = np.array([dose_info.ImagePositionPatient])
+	corner_v = np.array([ct_info.ImagePositionPatient])
+
+	x, y, z = np.meshgrid(np.arange(dim_vol[0]), np.arange(dim_vol[1]), np.arange(dim_vol[2]))
+	#xyz =  np.transpose(np.array([x, y, z]), (2, 1, 3, 0))
+	xyz_v = np.transpose(np.array([y, x, z]), (1, 2, 3, 0))
+
+
+	xyz_d = (xyz_v * dim_voxv + corner_v - corner_d)/dim_voxd
+	xyz_d = xyz_d.astype(int)
+
+	valid_voxel = np.logical_and(xyz_d[:,:,:,0] >= 0, xyz_d[:,:,:,1] >= 0)
+	valid_voxel = np.logical_and(valid_voxel, xyz_d[:,:,:,2] >= 0)
+	valid_voxel = np.logical_and(valid_voxel, xyz_d[:,:,:,0] < dim_dos[1])
+	valid_voxel = np.logical_and(valid_voxel, xyz_d[:,:,:,1] < dim_dos[0])
+	valid_voxel = np.logical_and(valid_voxel, xyz_d[:,:,:,2] < dim_dos[2])
+
+	xyz_v = xyz_v[valid_voxel]
+	xyz_d = xyz_d[valid_voxel]
+	dose_v = np.zeros(dim_vol)
+
+	#dose_v[xyz_v] = dosegrids[xyz]
+	for (i,posv) in enumerate(xyz_v):
+		dose_v[posv[1],posv[0],posv[2]] = dose_grids[xyz_d[i][1],xyz_d[i][0],xyz_d[i][2]]
+
+	return dose_v
 	
-	# Sum dose info for dose grid resolution file
-	dose_grids = load_rtdose_files(dose_files)
-	dose = np.sum(np.array(dose_grids), axis=0)
+'''
+Write the dose grid in NRRD format
+Parameters:
+	dose - the dose grid
+	directory - where to write the NRRD
+'''
+def write_dose(dose, directory):
+	filename = os.path.join(directory, 'dose_in_CT_dimensions.nrrd')
+	print('Writing ' + filename)
+	nrrd.write(filename, dose)
 
-	# Read CT as a SimpleITK image
-	reader = sitk.ImageSeriesReader()
-	reader.SetFileNames(ct_files)
-	ct = reader.Execute()
-	
-	# CT spacing is in wrong units?
-	ct_spacing = ct.GetSpacing()
-	ct_spacing = [ct_spacing[0], ct_spacing[1], ct_spacing[2]/1e2]
-	ct.SetSpacing(ct_spacing)
-
-
-	# Set up the resampler
-	resampler = sitk.ResampleImageFilter()
-	resampler.SetOutputSpacing(ct.GetSpacing())
-	resampler.SetSize(ct.GetSize())
-	resampler.SetOutputDirection(ct.GetDirection())
-	resampler.SetOutputOrigin(ct.GetOrigin())
-	resampler.SetTransform(sitk.Transform())
-	resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-
-	for dose_file in dose_files:
-		print()
-		print(dose_file)
-		# Read dose as SimpleITK image so we can SetOrigin(), etc
-		dose_sitk = sitk.ReadImage(dose_file)
-		print('dose.any() before resample')
-		print((sitk.GetArrayFromImage(dose_sitk)).any())
-
-		# Resample
-		resampled = resampler.Execute(dose_sitk)
-		dose_resampled = sitk.GetArrayFromImage(resampled)
-		dose_resampled = dose_resampled.transpose()
-		print('dose.any() after resample')
-		print(dose_resampled.any())
-
-		# Add to total resampled
-
-	return dose, dose_resampled
 
 if __name__=='__main__':
 	parser = argparse.ArgumentParser()
@@ -75,20 +97,15 @@ if __name__=='__main__':
 		ct_prefix = 'CT'
 		dose_prefix = 'RTDOSE'
 		
-		ct_files = find_prefixed_files(dcm_directory, ct_prefix)
-		dose_files = find_prefixed_files(dcm_directory, dose_prefix)
-
+		ct_infos = [pydicom.dcmread(f) for f in find_prefixed_files(dcm_directory, ct_prefix)]
+		dose_info = pydicom.dcmread(find_prefixed_file(dcm_directory, dose_prefix))
+		dose_grids = load_rtdose_files(find_prefixed_files(dcm_directory, dose_prefix))
 	except Exception as ex:
 		print(type(ex), ex)
 		print('Could not load in ct/dose info')
 		exit(0)
 	
-	dose, dose_resampled = resample_dose(ct_files, dose_files)
+	dose = resample_dose(ct_infos, dose_info, dose_grids)
+	# dose = np.transpose(dose,(1,0,2))
 
-	filename = os.path.join(args.directory, 'dose_in_CT_dimensions.nrrd')
-	print('Writing ' + filename)
-	nrrd.write(filename, dose_resampled)
-
-	filename = os.path.join(args.directory, 'dose.nrrd')
-	print('Writing ' + filename)
-	nrrd.write(filename, dose)
+	write_dose(dose, args.directory)
